d66c47e0e2f64e0227694be172a00bc89bbc1374
Lopito92 <mflopo92@gmail.com>
2026-04-08 15:33:50 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,4 @@
-const CACHE_NAME = 'bdnc-padel-v1';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(['./']))
-  );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', e => {
-  // Network first, fallback to cache
-  e.respondWith(
-    fetch(e.request)
-      .catch(() => caches.match(e.request))
-  );
-});
+// Minimal SW - just enough for PWA installability
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+// No fetch handler - don't intercept anything
